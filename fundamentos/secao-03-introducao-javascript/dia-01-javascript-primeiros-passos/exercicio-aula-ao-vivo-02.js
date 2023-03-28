167a15f9b7169ec2e742f68dbd2c646b5bc60dda
Alex Vido <alexvidodev@gmail.com>
2023-03-28 18:04:50 -0300

    Exercicios feitos em grupo na aula ao vivo

---
+++ fundamentos/secao-03-introducao-javascript/dia-01-javascript-primeiros-passos/exercicio-aula-ao-vivo-02.js
@@ -0,0 +1,8 @@
+/* Você fez um ótimo trabalho durante o mês e irá receber um bônus salarial :D
+Calcule qual será seu salário final, sabendo que o salário base é 3500 e o bônus será de 500.
+Exiba o resultado no console. */
+
+const salario = 3500;
+const bonus = 500;
+
+console.log(salario + bonus);
