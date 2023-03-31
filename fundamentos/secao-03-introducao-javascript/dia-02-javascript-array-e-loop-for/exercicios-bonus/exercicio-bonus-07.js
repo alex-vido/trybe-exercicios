786c73a50af1bd56642d63ceb88c7dbf1c87f2a7
Alex Vido <alexvidodev@gmail.com>
2023-03-31 17:58:27 -0300

    Feito correção no exercicio 7

--- fundamentos/secao-03-introducao-javascript/dia-02-javascript-array-e-loop-for/exercicios-bonus/exercicio-bonus-07.js
+++ fundamentos/secao-03-introducao-javascript/dia-02-javascript-array-e-loop-for/exercicios-bonus/exercicio-bonus-07.js
@@ -1,19 +1,21 @@
-let n = 7
+let n = 5
+let draw = '';
 let middle = (n + 1) / 2; // 3
 let left = middle;
 let right = middle;
 
-for (let index = 1; index <= middle; index += 1) {
-  let draw = '';
-  for (let secondIndex = 1; secondIndex <= n; secondIndex += 1) {
-    if (secondIndex == left || secondIndex == right || index == middle) { // Se 2index for maior que 3 e
+for (let index = 0; index <= middle; index += 1) {
+  for (let secondIndex = 0; secondIndex <= n; secondIndex += 1) {
+    if (secondIndex > left && secondIndex < right) { // Se 2index for maior que 3 e
       draw += '*';
     } else {
       draw += ' ';
     }
   }
-  
+
+  draw+= '\n'
   right += 1;
   left -= 1;
-  console.log(draw);
 }
+
+console.log(draw);
