b4b4ee8aac303f14115bd92c8af9f6215b7e21b7
Alex Vido <alexvidodev@gmail.com>
2023-04-03 13:34:45 -0300

    Reduzido para 1 for exercicio-bonus-5

--- fundamentos/secao-03-introducao-javascript/dia-02-javascript-array-e-loop-for/exercicios-bonus/exercicio-bonus-05.js
+++ fundamentos/secao-03-introducao-javascript/dia-02-javascript-array-e-loop-for/exercicios-bonus/exercicio-bonus-05.js
@@ -1,11 +1,9 @@
 let n = 5
 let desenho = '';
 
-for (let index = 1; index <= n; index += 1) {
-  desenho += '\n';
-  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-    desenho += '*'
-  }
-}
+for (let index = 0; index < n; index += 1) {
 
-console.log(desenho);
+  desenho += '*'
+  console.log(desenho);
+
+}
