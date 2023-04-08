df45267e075499a2d281b3180c430a5d91e7da9d
Alex Vido <alex.vido93@gmail.com>
2023-04-08 18:20:33 -0300

    Tentando fazer exercicio numeros romanos

--- fundamentos/secao-03-introducao-javascript/dia-04-javascript-objetos/exercicios/exercicio-04.js
+++ fundamentos/secao-03-introducao-javascript/dia-04-javascript-objetos/exercicios/exercicio-04.js
@@ -1,3 +1,57 @@
+const converteAlgarismosRomanos = (str) => {
+  let n = 0;
+  for (let i = 1; i <= str.length; i += 1) {
+    const numerosRomanos = [
+      { name: 'I',
+        value: 1
+      },
+      { name: 'IV',
+        value: 4
+      },
+      { name: 'V',
+        value: 5
+      },
+      {name: 'IX',
+        value: 9
+      },
+      {name: 'X',
+        value: 10 
+      },
+      { name: 'XL',
+        value: 40
+      },
+      { name: 'L',
+        value: 50
+      },
+      { name: 'XC',
+        value: 90
+      },
+      { name: 'C',
+        value: 100 
+      },
+      { name: 'CD',
+        value: 400
+      },
+      { name: 'D',
+        value: 500 
+      },
+      { name: 'CM',
+        value: 900
+      },
+      { name: 'M',
+        value: 1000 
+      }
+    ]
+    if (Object.values(numerosRomanos[str[i - 1]]) < Object.values(numerosRomanos[str[i]])) {
+      n += (Object.values(numerosRomanos[str[i - 1]]).value + Object.values(numerosRomanos[str[i]].value));
+    }
+  }
+  return n
+}
+
+console.log(converteAlgarismosRomanos('IX'));
+
+
 // Exercício 02
 
 let vector = [[1, 2], [3,4,5,6], [7,8,9,10]];
@@ -16,7 +70,7 @@ const arrayOfNumbers = (vector) => {
 
 console.log(arrayOfNumbers(vector));
 
-//Exercício 03
+// Exercício 03
 
 let basket = [
   'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
@@ -63,6 +117,28 @@ const repeteMoreTimes = (func) => {
   return moreTimes;
 }
 
-
-
 console.log(repeteMoreTimes(repeteCounter(basket)));
+
+// Exercício 04
+
+let lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+let lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+let lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+let allLessons = Object.assign({}, { lesson1, lesson2, lesson3 })
