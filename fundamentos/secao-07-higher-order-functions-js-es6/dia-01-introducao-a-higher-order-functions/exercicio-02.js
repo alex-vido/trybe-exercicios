be011276cb77bf8187a69d9e2d0637c9727f58af
Alex Vido <alex.vido93@gmail.com>
2023-05-08 15:17:22 -0300

    Feito exercício 01

--- fundamentos/secao-07-higher-order-functions-js-es6/dia-01-introducao-a-higher-order-functions/exercicio-02.js
+++ fundamentos/secao-07-higher-order-functions-js-es6/dia-01-introducao-a-higher-order-functions/exercicio-02.js
@@ -1,5 +1,5 @@
 const sorter = (number) => {
-  const loterySorter = Math.round(Math.random() * 5);
+  const loterySorter = Math.floor((Math.random() * 5) + 1);
   let isCorrect = number === loterySorter ? `${loterySorter} Parabéns, você ganhou` : `${loterySorter} Tente Novamente`;
   return isCorrect;
 }
--- fundamentos/secao-07-higher-order-functions-js-es6/dia-01-introducao-a-higher-order-functions/exercicio-03.js
+++ fundamentos/secao-07-higher-order-functions-js-es6/dia-01-introducao-a-higher-order-functions/exercicio-03.js
@@ -1,6 +1,14 @@
 const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
-const notes = (rightAnswers, studentAnswers, callback) {
-
+const notes = (rightAnswers, studentAnswers, callback) => {
+  let counter = 0;
+  for (let i = 0; i < rightAnswers.length; i += 1) {
+    if (studentAnswers[i] === 'N.A') {
+    } else if (studentAnswers[i] !== rightAnswers) {
+      counter -= 0.5;
+    } else {
+      counter += 1;
+    }
+  }
 }
