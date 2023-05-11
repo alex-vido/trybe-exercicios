8e4fb8421827612e71911baea0d99cf73a13a35a
Alex Vido <alex.vido93@gmail.com>
2023-05-11 15:03:27 -0300

    Feito exercício 3

--- fundamentos/secao-07-higher-order-functions-js-es6/dia-01-introducao-a-higher-order-functions/exercicio-03.js
+++ fundamentos/secao-07-higher-order-functions-js-es6/dia-01-introducao-a-higher-order-functions/exercicio-03.js
@@ -1,6 +1,9 @@
-const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const RIGHT_ANSWERS =  ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
-const notes = (rightAnswers, studentAnswers, callback) {
+const notes = (rightAnswers) => rightAnswers.reduce((acc, curr, index) => 
+  STUDENT_ANSWERS[index] === curr ? acc + 1
+  : STUDENT_ANSWERS[index] === 'N.A' ? acc 
+  : acc - 0.5, 0);
 
-}
+console.log(notes(RIGHT_ANSWERS));
