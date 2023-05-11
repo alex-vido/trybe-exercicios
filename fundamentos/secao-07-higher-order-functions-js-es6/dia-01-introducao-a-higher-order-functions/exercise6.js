5469837459e5e7c199bb09d9635a8db4815b8b9a
Alex Vido <alex.vido93@gmail.com>
2023-05-11 15:45:11 -0300

    Feito exercício 6

---
+++ fundamentos/secao-07-higher-order-functions-js-es6/dia-01-introducao-a-higher-order-functions/exercise6.js
@@ -0,0 +1,16 @@
+const books = require('./books');
+
+const expectedResult = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
+
+const getNamedBook = (ar) => ar.find(({ name }) => name.length === 26);
+
+console.log(getNamedBook(books));
