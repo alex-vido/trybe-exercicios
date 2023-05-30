770e44b3bdd6e4300bfe378011cf3b0736cf4ba6
Alex Vido <alex.vido93@gmail.com>
2023-05-30 18:19:35 -0300

    Feito exercício do dia

---
+++ front-end/secao-02-introducao-ao-react/dia-03-avancando-em-componentes/books/src/assets/data.ts
@@ -0,0 +1,42 @@
+const booksList = [
+  {
+    title: 'The Godfather',
+    pages: 433,
+    isRead: true,
+    isFavorite: false,
+  },
+  {
+    title: 'The Little Prince',
+    pages: 92,
+    isRead: true,
+    isFavorite: true,
+  },
+  {
+    title: 'Clean Code',
+    pages: 425,
+    isRead: false,
+    isFavorite: false,
+  },
+  {
+    title: 'Dom Casmurro',
+    pages: 192,
+    isRead: true,
+    isFavorite: false,
+  },
+  {
+    title: 'Vidas Secas',
+    pages: 176,
+    isRead: true,
+    isFavorite: false,
+  },
+  {
+    title: 'Turma da Mônica',
+    pages: 52,
+    isRead: false,
+    isFavorite: false,
+  },
+];
+
+export const bestSerie = 'Two and a Half Men';
+
+export default booksList;
