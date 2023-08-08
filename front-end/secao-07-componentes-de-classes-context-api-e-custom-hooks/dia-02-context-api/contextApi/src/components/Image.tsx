08e077bd97fd628d0e13259f1e9bd2971e5720f4
Alex Vido <alex.vido93@gmail.com>
2023-08-08 15:51:09 -0300

    Day lesson

---
+++ front-end/secao-07-componentes-de-classes-context-api-e-custom-hooks/dia-02-context-api/contextApi/src/components/Image.tsx
@@ -0,0 +1,11 @@
+import trybeImage from '../../public/trybe.svg';
+
+function Image() {
+  return (
+    <div>
+      <img src={trybeImage} alt="trybe logo" />
+    </div>
+  );
+}
+
+export default Image;
